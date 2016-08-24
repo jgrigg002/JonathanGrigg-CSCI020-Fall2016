d979a176c5eed071760f68121140942df6d4f4cc
Jonathan Grigg <jgrigg002@student.butte.edu>
2016-08-24 18:07:52 +0000

    Ascii Art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,12 @@
+/* Creator:Jonathan Grigg
+Date:08/24/16
+
+Purpose:Art*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout<<"~~~~~"<<endl<<" ^^ |"<<endl<<" <  "<<endl<<" __ "<<endl<<"[>o<]";
+}
