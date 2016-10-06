3bca2e11d829f17d4e179a16f1515631d7414bfb
Jonathan Grigg <jgrigg002@student.butte.edu>
2016-10-06 05:08:29 +0000

    Lab11

---
+++ lab11/lab11.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+double calc_gross_income(double wages, double interest, double unemployment,int status){         //calc gross income
+    double income;
+    income = wages+interest+unemployment;
+    if (status = 2){
+        
+    }
+    else {
+        
+    }
+    
+    return income;
+}
+
+int main (){
+    int marital                         //marital status
+    double withold                      //amount withheld for taxes
+    double g_wages;                     //money made at work
+    double g_interest;                  //made in interest
+    double g_unemp;                     //from unemployment
+    string name;                        //name stored here
+    
+return 0;
+}
