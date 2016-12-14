ca4889c7de0d6d42e95bcdd8e05921e79606f681
Jonathan Grigg <jgrigg002@student.butte.edu>
2016-12-14 09:01:33 +0000

    halfway done with probability generator

--- assignment5/assignment5.cpp
+++ assignment5/assignment5.cpp
@@ -2,6 +2,8 @@
 //csci20 final project
 //12.13.16
 
+//to fix finish probability generator and creat output functions so user can choose a number to generate
+
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -58,7 +60,7 @@ class city{
     string get_name();
     pop get_pop(); //gets pop data
     income get_in(); //gets income data
-    ethn get_eth(); //gets income data
+    ethn get_eth(int e); //gets income data
 };
 
 int main(){
@@ -83,8 +85,9 @@ int main(){
     income i_in;
     income i_out;
     ethn e_in[99];
-    ethn e_out;
+    ethn e_out[99];
     
+    float ran_gen = 0;
     
     in_key.open("input_key.txt"); //input is opened
     
@@ -113,17 +116,17 @@ int main(){
         //opens file
         else{
         in_file.open(file_name_in);
-        if (in_file.is_open()){
-            cout<<"file is open"<<endl;
-        }
+        //if (in_file.is_open()){ //for testing
+        //    cout<<"file is open"<<endl;
+        //}
         }
     }
     //opens file if user doesn't want to read the key
     else{
         in_file.open(file_name_in);
-        if (in_file.is_open()){
-            cout<<"file is open"<<endl;
-        }
+       // if (in_file.is_open()){
+    //        cout<<"file is open"<<endl;
+      //  }
     }
     //I close out the inout key file here so I can close the program if things go wrong
     in_key.close();
@@ -147,7 +150,7 @@ int main(){
             //inputs the population struct
             case 'p': 
                 iss>> p_in.pop;
-                for(int j =0 ; j<6; j++){   
+                for (int j =0 ; j<6; j++){   
                     iss>>choice;
                     switch (choice){
                         case 'b':
@@ -173,33 +176,31 @@ int main(){
                         case 'm':
                         iss>> p_in.male;
                         break;
+                        
+                        default:
+                        cout<<"error";
                     }
                 }
-                cout<<p_in.baby<<endl;
-                cout<<p_in.child<<endl;
-                cout<<p_in.adult<<endl;
-                cout<<p_in.senior<<endl;
-                cout<<p_in.female<<endl;
-                cout<<p_in.male<<endl;
-                
+
                 city1.set_pop(p_in);
             break;
             
             //ethnicity data
             case 'e':
                 iss>>eth;
+                //cout<<eth<<endl;   //testing ethnicity number input
                 city1.set_num_eth(eth);
                 for (int k =0; k < eth; k++){
                     iss>> e_in[k].name;
                     iss>> e_in[k].percent;
                     city1.set_eth(e_in[k]);
                 }
-                
             break;
             
             //income data
             case 'i':
             for (int l = 0; l < 4; l++){
+                iss>>choice;
                 switch (choice){
                     case 'm':
                     iss>> i_in.male;
@@ -213,14 +214,20 @@ int main(){
                     case 'h':
                     iss>> i_in.no_insurance;
                     break;
+                    default:
+                    cout<<"error"<<endl;
                 }
             }
             city1.set_income(i_in);
             break;
+            
+            default:
+            cout<<"base error"<<endl;
         }
     } 
     in_file.close();
-    
+
+ //for testing output from class
     cout<<city1.get_name()<<endl;
     p_out = city1.get_pop();
     cout<<p_out.pop<<endl;
@@ -232,16 +239,57 @@ int main(){
     cout<<p_out.senior<<endl;
 
     i_out = city1.get_in();
+    //output test
     cout<<i_out.male<<endl;
     cout<<i_out.female<<endl;
     cout<<i_out.poverty<<endl;
     cout<<i_out.no_insurance<<endl;
     
+    //for testing output of ethnicity
+    
     for (int m =0; m < eth; m++){
-    e_out = city1.get_eth();
-    cout<<e_out.name<<endl;
-    cout<<e_out.percent<<endl;
+    e_out[m] = city1.get_eth(m);
     }
+    
+    for (int r =0; r< eth; r++){
+        cout<<e_out[r].name<<endl;
+        cout<<e_out[r].percent<<endl;
+    }
+    
+    srand(time(0));
+    ran_gen = (rand()%1000);
+    if(ran_gen<(p_out.female * 10)){
+        cout<<"person is female, ";
+
+    }
+    else{
+        cout<<"person is male, ";
+
+    }
+    
+    srand(time(0));
+    ran_gen = (rand()%1000);
+    if (ran_gen < (p_out.baby * 10)){
+        cout<<"is a baby, ";
+    }
+    else if((ran_gen > (p_out.baby * 10)) && (ran_gen <((p_out.baby + p_out.child) * 10))){
+        cout<<"is a child, ";
+    }
+    else if ((ran_gen > ((p_out.baby + p_out.child) * 10)) && (ran_gen <((p_out.baby + p_out.child + p_out.adult) * 10))){
+        cout<<"is an adult, ";
+    }
+    else {
+        cout <<"is a senior, ";
+    }
+    /*
+    srand(time(0));
+    ran_gen = (rand()%eth);
+    int a = ran_gen;
+    switch (a)
+    */
+    
+    
+    
     return 0;
 }
 
@@ -292,7 +340,7 @@ income city::get_in(){
     return c_inc;
 }
 
-ethn city::get_eth(){
-    counter--;
-    return c_eth[counter];
+ethn city::get_eth(int e){
+    //counter--;
+    return c_eth[e];
 }
